50696bd6f48452cc4b739f654c93dff5b1be9390
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-20 04:59:47 +0530

    Add code for inserting number at random position of matrix

--- code.cpp
+++ code.cpp
@@ -4,18 +4,31 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
+    int n = 4, i, j;
     vector<vector<int>> v(n, vector<int>(n, 0));
-
+    srand(time(0));
     cout << "Welcome to the 2048 Game!" << endl;
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (j = 0; j < n; j++)
         {
             cout << v[i][j] << " ";
         }
         cout << endl;
     }
     cout << endl;
+    i = rand() % n;
+    j = rand() % n;
+    v[i][j] = 2;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
     return 0;
 }
