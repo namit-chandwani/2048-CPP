780fb7c204dcd7483664716a887c1e5d83724f1c
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-20 04:53:02 +0530

    Initialize and print  matrix with 0 values

--- code.cpp
+++ code.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
+    int n = 4;
+    vector<vector<int>> v(n, vector<int>(n, 0));
+
     cout << "Welcome to the 2048 Game!" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     return 0;
 }
