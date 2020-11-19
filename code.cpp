312f733f74ce06723ffaed953786859651412ac7
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-20 05:04:10 +0530

    Modularize matrix print code to function

--- code.cpp
+++ code.cpp
@@ -2,33 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printMatrix(vector<vector<int>> &v, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 int main()
 {
     int n = 4, i, j;
     vector<vector<int>> v(n, vector<int>(n, 0));
     srand(time(0));
     cout << "Welcome to the 2048 Game!" << endl;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    printMatrix(v, n);
     i = rand() % n;
     j = rand() % n;
     v[i][j] = 2;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    printMatrix(v, n);
 
     return 0;
 }
